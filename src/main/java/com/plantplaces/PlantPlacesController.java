1656d656c894533426e4c2b233a65cd9ef73b889
kylerseverance <67241123+kylerseverance@users.noreply.github.com>
2021-09-09 14:40:31 -0400

    Update / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,11 +3,30 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Handle the /start endpoint
+ * @return
+ *
+ */
 @Controller
 public class PlantPlacesController {
 	
+	
+	
 	@RequestMapping("/start")
 	public String start() {
+		
+	 return "start";
+	}
+	
+	/**
+	 * Handle the /start endpoint
+	 * @return
+	 *
+	 */
+	
+	@RequestMapping("/start")
+	public String index() {
 		
 	 return "start";
 	}
